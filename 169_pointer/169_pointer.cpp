bd81ff8e580485eb0f2d94cb8886cf5a869c9c36
Ryvqi <rifqikhabibi03@gmail.com>
2023-05-08 14:54:45 +0700

    memebuat prosedure

--- 169_pointer/169_pointer.cpp
+++ 169_pointer/169_pointer.cpp
@@ -6,6 +6,10 @@ public:
     int nim;
     void showNim();  //deklasi method
 };
+
+void mahasiswa::showNim() {  //implementasi method diluar class
+    cout << "No Induk = " << nim << endl;
+}
 int main()
 {
     
